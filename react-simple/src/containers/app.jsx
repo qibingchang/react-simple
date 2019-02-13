0b9d49038463e8943c43c8f82b05e0ce7e802af2
qibingchang <42823965+qibingchang@users.noreply.github.com>
2019-02-13 09:17:41 +0800

    Add files via upload

---
+++ react-simple/src/containers/app.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "comp/header";
+import Layout from "comp/layout";
+
+import "common/css/bootstrap.min.css";
+import "common/css/reset.css";
+import "common/css/common.css";
+import "common/css/style.css";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return <div className="wraper">
+            <Header></Header>
+            <Layout>
+                this is enter page
+            </Layout>
+        </div>
+    }
+}
+export default App;
